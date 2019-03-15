fb3f614d8beeb0b7671c65ef4d75a43e57047608
Jen Chan <6406037+usrrname@users.noreply.github.com>
2019-03-15 16:26:35 -0400

    updated copy and bootstrap cols

--- src/components/Story.vue
+++ src/components/Story.vue
@@ -1,17 +1,35 @@
 <template>
-    <div class="container">
-        <div class="row">
-        <section>
-            <img src="@/assets/img/team.png" width="90%">
-            <p class='lead'>A little bit of art, a little bit of “science”. 
+  <div class="container">
+    <div class="row">
+      <section class="col-md-12">
+        <img src="@/assets/img/team.png" width="90%">
+
+        <div class="col-md-8">
+          <p class="lead">
+            A little bit of art, a little bit of “science”.
             We’re four artists taking user research/testing approach to making life for cats better.
-            </p>
-        </section>
+            At one point or another, they all worked out of a DIY studio in Toronto called
+            <a href="http://brandscape.club" target="_blank">The Brandscape</a>
+          </p>
+        </div>
+        <div
+          class="col-md-8"
+        ><a href="https://www.instagram.com/samchampion_/" target="_blank">Sam's</a> human is Cat Bluemke, an artist and designer and one half of Spek Work, an art-game studio Cat started with Jon.</div>
+        <div
+          class="col-md-8"
+        >Jon is a an autodidact game designer who has supported many non-profit art organizations with developing digital solutions.</div>
+        <div
+          class="col-md-8"
+        >Wiener's human is Jen, an artist and web developer who decided to start this project about what cats want from art and entertainment. She supports her practice through tech support and various gigs.</div>
+        <div
+          class="col-md-8"
+        >Ben McCarthy coexists with Casio, a rescue cat. He is a musician, sound designer and teacher who runs a podcast, Precariat Content. He also does sound tech for socially transformative projects and theatre productions.</div>
+      </section>
     </div>
-    </div>
+  </div>
 </template>
 <script>
 export default {
-    name: 'story'
-}
+  name: "story"
+};
 </script>
